9fc9437cb6e26f01a9b55bcbe712b62ceee26a38
Devessier <baptiste@devessier.engineer>
2019-01-11 10:52:50 +0100

    Continued minishellv2

--- include/sh.h
+++ include/sh.h
@@ -6,7 +6,7 @@
 /*   By: bdevessi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/10 20:29:24 by bdevessi          #+#    #+#             */
-/*   Updated: 2019/01/10 23:44:00 by bdevessi         ###   ########.fr       */
+/*   Updated: 2019/01/11 10:51:12 by bdevessi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,16 @@ typedef struct	s_reader
 	size_t	index;
 }				t_reader;
 
+typedef struct	s_command
+{
+	size_t	len;
+	size_t	cap;
+	char	*str;
+}				t_command;
+
 void			reader_init(t_reader *this);
-ssize_t			reader_getchar(t_reader *this, const int fd, char *c);
+void			command_init(t_reader *this;
+int				sh_command_cat(t_command *this, char c);
+ssize_t			sh_getchar(t_reader *this, const int fd, char *c);
 
 #endif
--- srcs/reader.c
+++ srcs/reader.c
@@ -6,7 +6,7 @@
 /*   By: bdevessi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/10 20:41:45 by bdevessi          #+#    #+#             */
-/*   Updated: 2019/01/10 23:46:39 by bdevessi         ###   ########.fr       */
+/*   Updated: 2019/01/11 10:50:48 by bdevessi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,37 @@ void	reader_init(t_reader *this)
 		.len = 0,
 		.index = 0
 	};
+}
+
+void	command_init(t_command *this)
+{
+	*this = (t_command) {
+		.len = 0,
+		.cap = 0,
+		.str = NULL
+	};
+}
+
+int		sh_command_cat(t_command *this, char c)
+{
+	const size_t	remaining = this->cap - this->len;
+	char			*tmp;
+	size_t			cap;
+
+	if (remaining < 1)
+	{
+		cap = this->cap == 0 ? 10 : this->cap;
+		tmp = this->str;
+		cap *= cap > 1024 ? 1.5 : 2;
+		if (this->str = ft_strnew(cap))
+			return (-1);
+		this->cap = cap;
+		if (tmp != NULL)
+			ft_strcpy(this->str, tmp);
+		free(tmp);
+	}
+	this->str[this->len++] = c;
+	return (0);
 }
 
 ssize_t	sh_getchar(t_reader *this, const int fd, char *c)
