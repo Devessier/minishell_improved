9aa82423f799e663df6118642c76f346e6b061bf
Baptiste DEVESSIER <bdevessi@student.42.fr>
2019-02-07 13:07:45 +0100

    Expand tildes and variables after the lexer has done its job

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -6,7 +6,7 @@
 /*   By: bdevessi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/15 09:30:25 by bdevessi          #+#    #+#             */
-/*   Updated: 2019/02/05 23:50:26 by bdevessi         ###   ########.fr       */
+/*   Updated: 2019/02/07 13:05:50 by bdevessi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,82 +60,6 @@ void						copy_lexer(t_lexer *new, t_lexer *old)
 	free(new);
 }
 
-size_t						append_token_env_var(t_oken *tok, char *str, t_env *env)
-{
-	size_t		i;
-	t_string	*env_var;
-	char		*tmp;
-	char		old;
-
-	++str;
-	if (*str == '$')
-	{
-		if (*shell_pid == '\0')
-			ft_itoa_buff(getpid(), shell_pid);
-		ft_concat_strings(&tok->payload, shell_pid, ft_strlen(shell_pid));
-		return (1);
-	}
-	if (*str == '0')
-		if (!ft_concat_strings(&tok->payload, "minishell", 9))
-			; // malloc error
-	if (!(*str == '_' || ft_isalpha(*str)))
-		return (1);
-	i = 0;
-	while (i[str] && (ft_isalnum(i[str]) || i[str] == '_'))
-		i++;
-	old = str[i];
-	str[i] = '\0';
-	if (i > 0 && (env_var = get_env(env, str)) != NULL && env_var->buff != NULL)
-	{
-		tmp = ft_strchr(env_var->buff, '=');
-		ft_concat_strings(&tok->payload, tmp + 1, env_var->len - (tmp - env_var->buff) - 1);
-	}
-	str[i] = old;
-	return (i);
-}
-
-int							is_valid_username_char(int c)
-{
-	return (c != 0 && (ft_isalnum(c) || c == '_' || c == '-' || c == '.'));
-}
-
-size_t						append_tilde(t_oken *tok, char *str, t_env *env)
-{
-	char			home_dir[PATH_MAX];
-	t_string		*home;
-	struct passwd	*passwd;
-	size_t			i;
-
-	i = 0;
-	str++;
-	while (is_valid_username_char(*str))
-		home_dir[i++] = *str++;
-	home_dir[i] = '\0';
-	if (*home_dir == '\0')
-	{
-		if ((home = get_env(env, "HOME")) == NULL || home->len == 0)
-		{
-			if (!(passwd = getpwnam(getlogin())) || passwd->pw_dir == NULL)
-				return (0);
-			ft_strcpy(home_dir, passwd->pw_dir);
-		}
-		else
-			ft_strcpy(home_dir, ft_strchr(home->buff, '=') + 1);
-	}
-	else if ((passwd = getpwnam(home_dir)) != NULL && passwd->pw_dir != NULL)
-	{
-		i += ft_strlen(str);
-		ft_strcpy(home_dir, passwd->pw_dir);
-	}
-	else
-		return (0);
-	ft_strcat(home_dir, str);
-	if (!ft_concat_strings(&tok->payload, home_dir, ft_strlen(home_dir)))
-		return (-1);
-	ft_putf("i = %d\n", i);
-	return (i);
-}
-
 bool						append_token(t_lexer *this, size_t index, t_oken token)
 {
 	t_oken	*tmp;
@@ -160,10 +84,9 @@ bool						append_token(t_lexer *this, size_t index, t_oken token)
 	return (true);
 }
 
-bool						lexer_algorithm(t_lexer *lexer, uint8_t *str, t_env *env)
+bool						lexer_algorithm(t_lexer *lexer, uint8_t *str)
 {
 	t_oken_char	types[2];
-	size_t		tmp;
 	size_t		i;
 	t_oken		tok;
 
@@ -175,15 +98,7 @@ bool						lexer_algorithm(t_lexer *lexer, uint8_t *str, t_env *env)
 		*types = g_tokens[*str];
 		if (*str == '#')
 			break ;
-		if (*str == '$' && lexer->state != IN_SQUOTE)
-		{
-			tmp = append_token_env_var(&tok, (char *)str, env);
-			if (tmp > 0)
-				str += tmp;
-			else if (!ft_concat_strings(&tok.payload, "$", 1))
-				return (false);
-		}
-		else if (lexer->state == GLOBAL_SCOPE)
+		if (lexer->state == GLOBAL_SCOPE)
 		{
 			if (*types == T_ESCAPE)
 			{
@@ -218,13 +133,7 @@ bool						lexer_algorithm(t_lexer *lexer, uint8_t *str, t_env *env)
 			else
 			{
 				tok.type = T_WORD;
-				if (*str == '~' && tok.payload.len == 0)
-				{
-					if ((tmp = append_tilde(&tok, (char *)str, env)) < 0)
-						return (false);
-					str += tmp;
-				}
-				if ((!(*str == '~' && tok.payload.len == 0) || (*str == '~' && tok.payload.len == 0 && tmp == 0)) && !ft_concat_strings(&tok.payload, (char *)str, 1))
+				if (!ft_concat_strings(&tok.payload, (char *)str, 1))
 					return (false);
 			}
 		}
@@ -251,6 +160,82 @@ bool						lexer_algorithm(t_lexer *lexer, uint8_t *str, t_env *env)
 	return (true);
 }
 
+bool						expand_tildes(t_string *token, t_env *env)
+{
+	char			home_dir[PATH_MAX];
+	t_string		*home;
+	struct passwd	*passwd;
+	size_t			i;
+	char			*str;
+
+	str = token->buff + 1;
+	i = 0;
+	while (*str != '\0' && *str != '/')
+		home_dir[i++] = *str++;
+	home_dir[i] = '\0';
+	if (*home_dir == '\0')
+	{
+		if ((home = get_env(env, "HOME")) == NULL || home->len == 0)
+		{
+
+			if (!(passwd = getpwnam(getlogin())) || passwd->pw_dir == NULL)
+				return (false);
+			ft_strcpy(home_dir, passwd->pw_dir);
+		}
+		else
+			ft_strcpy(home_dir, ft_strchr(home->buff, '=') + 1);
+	}
+	else if ((passwd = getpwnam(home_dir)) != NULL && passwd->pw_dir != NULL)
+		ft_strcpy(home_dir, passwd->pw_dir);
+	else
+		return (false);
+	ft_strcat(home_dir, str);
+	if (!ft_extend_string(token, ft_strlen(home_dir)))
+		return (false);
+	ft_strcpy(token->buff, home_dir);
+	return (true);
+
+	}
+
+
+size_t						append_token_env_var(t_oken *tok, char *str, t_env *env)
+{
+	size_t		i;
+	t_string	*env_var;
+	char		*tmp;
+	char		old;
+
+	++str;
+	if (*str == '$')
+	{
+		if (*shell_pid == '\0')
+			ft_itoa_buff(getpid(), shell_pid);
+		ft_concat_strings(&tok->payload, shell_pid, ft_strlen(shell_pid));
+		return (1);
+	}
+	if (*str == '0')
+		if (!ft_concat_strings(&tok->payload, "minishell", 9))
+			; // malloc error
+	if (!(*str == '_' || ft_isalpha(*str)))
+		return (1);
+	i = 0;
+	while (i[str] && (ft_isalnum(i[str]) || i[str] == '_'))
+		i++;
+	old = str[i];
+	str[i] = '\0';
+	if (i > 0 && (env_var = get_env(env, str)) != NULL && env_var->buff != NULL)
+	{
+		tmp = ft_strchr(env_var->buff, '=');
+		ft_concat_strings(&tok->payload, tmp + 1, env_var->len - (tmp - env_var->buff) - 1);
+	}
+	str[i] = old;
+	return (i);
+}
+
+/*bool						expand_variables(t_lexer *lexer, t_env *env)
+{
+}*/
+
 bool						destroy_lexer(const t_lexer *lexer)
 {
 	size_t	i;
@@ -265,8 +250,17 @@ bool						destroy_lexer(const t_lexer *lexer)
 t_lexer						sh_lexer(t_string *string, t_env *env)
 {
 	t_lexer			lexer;
+	size_t		i;	
 
 	init_lexer(&lexer);
-	lexer_algorithm(&lexer, (uint8_t *)string->buff, env);
+	if (!lexer_algorithm(&lexer, (uint8_t *)string->buff))
+		; //error
+	i = 0;
+	while (i++ < lexer.len)
+		if (lexer.tokens[i - 1].type == T_WORD && *lexer.tokens[i - 1].payload.buff == '~')
+		{
+			if (!(expand_tildes(&lexer.tokens[i - 1].payload, env)))
+				; // error occured
+		}
 	return (lexer);
 }
