086452c65a87c0f4e6ebe644ff183ec4a7eaf53e
Baptiste DEVESSIER <bdevessi@student.42.fr>
2019-02-21 11:28:28 +0100

    Improved my putstr, now it does only one write syscall

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,14 +6,17 @@
 /*   By: bdevessi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 10:45:39 by bdevessi          #+#    #+#             */
-/*   Updated: 2018/11/12 10:46:45 by bdevessi         ###   ########.fr       */
+/*   Updated: 2019/02/21 11:24:28 by bdevessi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
+#include <unistd.h>
 
 void	ft_putstr_fd(char const *s, int fd)
 {
-	while (*s)
-		ft_putchar_fd(*s++, fd);
+	const size_t	len = ft_strlen(s);
+
+	write(fd, s, len);
 }
